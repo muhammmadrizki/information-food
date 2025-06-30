e43c7ea528068ad9d6f9d8dc73c2b46dfbcaa1bb
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-06-30 19:08:19 +0700

    added taste and data on food

--- src/data/foods.ts
+++ src/data/foods.ts
@@ -18,17 +18,41 @@ export const foods: Food[] = [
   {
     id: 3,
     name: "Mie Celor",
+    taste: "savory",
   },
   {
     id: 4,
     name: "Pindang Patin",
+    taste: "spicy",
   },
   {
     id: 5,
     name: "Pempek",
+    taste: "savory",
   },
   {
     id: 6,
     name: "Ragit",
+    taste: "savory",
+  },
+  {
+    id: 7,
+    name: "Tekwan",
+    taste: "savory",
+  },
+  {
+    id: 8,
+    name: "Model",
+    taste: "savory",
+  },
+  {
+    id: 9,
+    name: "Celimpungan",
+    taste: "savory",
+  },
+  {
+    id: 10,
+    name: "Kemplang",
+    taste: "savory",
   },
 ];
