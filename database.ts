0cdc3c907b97468b745e00b408defac3592f66b0
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-07-13 21:49:10 +0700

    added database.ts, and modify package.json and tsconfig.json

---
+++ database.ts
@@ -0,0 +1,25 @@
+// Optional, if needed
+// import "dotenv/config";
+
+import * as pg from "pg";
+
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+
+  // If using separate variables
+  // host: process.env.POSTGRES_HOST,
+  // user: process.env.POSTGRES_USER,
+  // password: process.env.POSTGRES_PASSWORD,
+  // database: process.env.POSTGRES_DB,
+  // port: Number(process.env.POSTGRES_PORT),
+});
+
+await client.connect();
+
+const res = await client.query("SELECT * FROM users");
+
+const users = res.rows;
+
+console.log({ users });
+
+await client.end();
